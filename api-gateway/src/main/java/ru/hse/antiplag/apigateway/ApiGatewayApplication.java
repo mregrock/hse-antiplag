2a400792a0b94a9a53b7548aa4855370e1d73867
mregrock <78217843+mregrock@users.noreply.github.com>
2025-05-21 23:18:39 +0300

    Add swagger and simple OpenAPI doc

--- api-gateway/src/main/java/ru/hse/antiplag/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/ru/hse/antiplag/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,28 @@ package ru.hse.antiplag.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
   public static void main(String[] args) {
       SpringApplication.run(ApiGatewayApplication.class, args);
+  }
+
+  /**
+   * Configures basic OpenAPI metadata.
+   *
+   * @return OpenAPI object with API title, version, and description.
+   */
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("HSE Antiplag API Gateway")
+            .version("v1.0")
+            .description("API Gateway for HSE Antiplag System. \n" +
+                "Handles file uploads, downloads, and analysis requests, routing them to appropriate microservices."));
   }
 } 
